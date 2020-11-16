f1412c8c7e2441996ba0e471c69b03c9c3284354
hydrone <1286772338@qq.com>
2020-11-16 16:28:30 +0800

    更新Combobox滚动条样式

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 #include <QtWidgets/QApplication>
 #include <qdesktopwidget.h>
 
+#ifdef QT_DEBUG
 #include "qsswatcher/qsswatcher.h"
+#endif // QT_DEBUG
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+#ifdef QT_DEBUG
     QssWatcher watcher;
+#endif // QT_DEBUG
 
     QtLogFilter w(QApplication::desktop());
     w.show();
